c50f205e1c671d68f8df48444870d6a50891111b
Cooderfy <134572153+ojefersoncode@users.noreply.github.com>
2025-09-25 01:05:44 -0300

    Update Footer.tsx

--- components/LandingPage/Footer.tsx
+++ components/LandingPage/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
       <div className="grid grid-cols-1 md:gap-8 md:grid-cols-3 lg:grid-cols-4">
         {/* Marca */}
         <Card className="border-none bg-transparent shadow-none max-w-sm">
-          <h1 className="text-xl font-bold">LinKarme</h1>
+          <h1 className="text-xl font-bold text-white">LinKarme</h1>
           <p className=" text-sm text-white/70 max-w-xs">
             Plataforma de encurtamento e rastreamento de links para
             empreendedores digitais.
@@ -17,7 +17,7 @@ export default function Footer() {
 
         {/* Plataforma */}
         <Card className="border-none bg-transparent shadow-none">
-          <h2 className="font-semibold">Plataforma</h2>
+          <h2 className="font-semibold text-white">Plataforma</h2>
           <div className=" flex flex-col gap-2 text-sm text-white/70">
             <span>Preços</span>
             <span>Afiliados</span>
@@ -27,7 +27,7 @@ export default function Footer() {
 
         {/* Legais */}
         <Card className="border-none bg-transparent shadow-none">
-          <h2 className="font-semibold">Legais</h2>
+          <h2 className="font-semibold text-white">Legais</h2>
           <div className=" flex flex-col gap-2 text-sm text-white/70">
             <span>Termos de serviços</span>
             <span>Política de privacidade</span>
@@ -35,8 +35,8 @@ export default function Footer() {
         </Card>
 
         {/* Contato */}
-        <Card className="p-4 border border-accent/50 bg-accent/20 shadow-none h-min">
-          <h2 className="font-semibold">Entre em contato</h2>
+        <Card className="p-4 border border-accent/50 bg-accent/40 dark:bg-accent/20 shadow-none h-min">
+          <h2 className="font-semibold text-white">Entre em contato</h2>
 
           <div className="flex flex-col gap-2">
             <p className="text-sm text-white/80">Email: contato@linkarme.com</p>
